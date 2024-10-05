475b4d19ac18b75be8f98e72cf6bc419ab29c12d
TheeJacobyyy <xadiwise@yahoo.com>
2024-10-05 15:31:57 -0400

    first commit

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,41 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+app.use("/customer/auth/*", function auth(req, res, next) {
+    
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+        return res.status(401).send("Authorization token is required.");
+    }
+
+    
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).send("Token not found.");
+    }
+
+    
+    verifyToken(token, function(err, user) {
+        if (err) {
+            return res.status(403).send("Invalid token.");
+        }
+        
+        
+        req.user = user;
+        next(); 
+    });
+
+
+
+function verifyToken(token, callback) {
+    
+    
+   
+    const mockUser = { id: 1, name: "John Doe" }; 
+    callback(null, mockUser); 
+}
+
 });
  
 const PORT =5000;
